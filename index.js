1f97e024b7b0c11eb5f5a85f2a934bbc3595a52e
EdikKapustnyak <zned003@gmail.com>
2025-03-13 21:45:41 +0000

    Deploying to gh-pages from @ EdikKapustnyak/goit-js-hw-11@d893814472e3535ca1fd412914f0d840dbbffddb 🚀

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-import{S as u,i as c,a as p}from"./assets/vendor-D5mnuR-h.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&s(i)}).observe(document,{childList:!0,subtree:!0});function n(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(t){if(t.ep)return;t.ep=!0;const o=n(t);fetch(t.href,o)}})();const l=document.querySelector(".gallery");function f(r){l.innerHTML="";const e=r.map(s=>`
+import{S as u,i as n,a as d}from"./assets/vendor-D5mnuR-h.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const l=document.querySelector(".gallery");function p(a){l.innerHTML="";const s=a.map(r=>`
         <li class="gallery-item">
-            <a class="gallery-link" href="${s.largeImageURL}">
-                <img src="${s.webformatURL}" alt="${s.tags}" width="360" height="240" />
+            <a class="gallery-link" href="${r.largeImageURL}">
+                <img src="${r.webformatURL}" alt="${r.tags}" width="360" height="240" />
             </a>
             <div class="description">
                 <p class="gallery-item-text">Likes</p>
-                <span>${s.likes}</span>
+                <span>${r.likes}</span>
             </div>
             <div class="description">
                 <p class="gallery-item-text">Views</p>
-                <span>${s.views}</span>
+                <span>${r.views}</span>
             </div>
             <div class="description">
                 <p class="gallery-item-text">Comments</p>
-                <span>${s.comments}</span>
+                <span>${r.comments}</span>
             </div>
             <div class="description">
                 <p class="gallery-item-text">Downloads</p>
-                <span>${s.downloads}</span>
+                <span>${r.downloads}</span>
             </div>
-        </li>`).join("");l.innerHTML=e,new u(".gallery a",{captionsData:"alt",captionDelay:250}).refresh()}const m="0aad6a646770250f1e2d10aace71581f",a={};async function y(){try{const e=await(await fetch(`http://api.ipstack.com/check?access_key=${m}`)).json();a.ip=e.ip,a.country=e.country_name,a.city=e.city,a.latitude=e.latitude,a.longitude=e.longitude,console.log("Информация о пользователе:",a),h(a)}catch(r){console.error("Ошибка при получении IP-адреса:",r)}}function h(r){fetch("https://your-server.com/api/save-user-info",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(r)}).then(e=>e.json()).then(e=>{console.log("Данные успешно отправлены на сервер:",e)}).catch(e=>{console.error("Ошибка при отправке данных на сервер:",e)})}window.addEventListener("load",y);const g=document.querySelector(".form");g.addEventListener("submit",d);function d(r){r.preventDefault();const e=document.querySelector('input[name="search-text"]').value.trim();if(e===""){c.show({title:"Error",message:"Please enter a search query!"});return}v(e).then(n=>{n.data.hits.length===0?c.show({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):(console.log(n.data),f(n.data.hits))}).catch(n=>{console.error(n),c.show({title:"Error",message:"Sorry, there was an error processing your request. Please try again!"})})}function v(r){return p.get("https://pixabay.com/api/",{params:{key:"49320414-34c235123b434a4e0d748057d",q:r,image_type:"photo",orientation:"horizontal",safesearch:!0}})}const w=document.querySelector(".form");w.addEventListener("submit",d);
+        </li>`).join("");l.innerHTML=s,new u(".gallery a",{captionsData:"alt",captionDelay:250}).refresh()}const m=document.querySelector(".form");m.addEventListener("submit",c);function c(a){a.preventDefault();const s=document.querySelector('input[name="search-text"]').value.trim();if(s===""){n.show({title:"Error",message:"Please enter a search query!"});return}f(s).then(o=>{o.data.hits.length===0?n.show({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}):(console.log(o.data),p(o.data.hits))}).catch(o=>{console.error(o),n.show({title:"Error",message:"Sorry, there was an error processing your request. Please try again!"})})}function f(a){return d.get("https://pixabay.com/api/",{params:{key:"49320414-34c235123b434a4e0d748057d",q:a,image_type:"photo",orientation:"horizontal",safesearch:!0}})}const y=document.querySelector(".form");y.addEventListener("submit",c);
 //# sourceMappingURL=index.js.map
