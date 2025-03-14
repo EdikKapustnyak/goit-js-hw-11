009d4fd0939a13333829136959ad0424535be756
kapustnyak <zned003@gmail.com>
2025-03-14 15:31:10 +0100

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,51 +1,12 @@
 import axios from 'axios';
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import { renderGallery } from './render-functions';
 
-const form = document.querySelector('.form');
-const loader = document.querySelector('.loader')
-// form.addEventListener('submit', findImg);
-
-export function findImg(event) { 
-    event.preventDefault();
-    
-    const getInputValue = document.querySelector('input[name="search-text"]').value.trim();
-
-    if (getInputValue === '') { 
-        iziToast.show({
-            title: 'Error',
-            message: 'Please enter a search query!'
-        });
-        return;
-    }
-    loader.classList.remove('visually-hidden');
-    getRequest(getInputValue)
-        .then(response => {
-            if (response.data.hits.length === 0) { 
-                iziToast.show({
-                    title: 'Error',
-                    message: 'Sorry, there are no images matching your search query. Please try again!'
-                });
-            } else {
-                console.log(response.data);
-                renderGallery(response.data.hits);
-            }
-        })
-        .catch(error => {
-            console.error(error);
-            iziToast.show({
-                title: 'Error',
-                message: 'Sorry, there was an error processing your request. Please try again!'
-            });
-        })
-        .finally(() => loader.classList.add('visually-hidden'));
-}
+const API_URL = 'https://pixabay.com/api/';
+const MY_KEY = '49320414-34c235123b434a4e0d748057d';
 
 export function getRequest(request) {
-    return axios.get('https://pixabay.com/api/', {
+    return axios.get(API_URL, {
         params: { 
-            key: '49320414-34c235123b434a4e0d748057d',
+            key: MY_KEY,
             q: request,
             image_type: 'photo',
             orientation: 'horizontal',
--- src/main.js
+++ src/main.js
@@ -1,8 +1,46 @@
-import './js/pixabay-api';
-import './js/render-functions';
-import { findImg, getRequest } from './js/pixabay-api';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { getRequest } from './js/pixabay-api';
 import { renderGallery } from './js/render-functions';
 
-const form = document.querySelector(`.form`);
+const form = document.querySelector('.form');
+const loader = document.querySelector('.loader');
 
 form.addEventListener('submit', findImg);
+
+function findImg(event) { 
+    event.preventDefault();
+    
+    const getInputValue = document.querySelector('input[name="search-text"]').value.trim();
+
+    if (getInputValue === '') { 
+        iziToast.show({
+            title: 'Error',
+            message: 'Please enter a search query!'
+        });
+        return;
+    }
+
+    loader.classList.remove('visually-hidden');
+
+    getRequest(getInputValue)
+        .then(response => {
+            if (response.data.hits.length === 0) { 
+                iziToast.show({
+                    title: 'Error',
+                    message: 'Sorry, there are no images matching your search query. Please try again!'
+                });
+            } else {
+                console.log(response.data);
+                renderGallery(response.data.hits);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            iziToast.show({
+                title: 'Error',
+                message: 'Sorry, there was an error processing your request. Please try again!'
+            });
+        })
+        .finally(() => loader.classList.add('visually-hidden'));
+}
